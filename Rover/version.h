2ff795b650c6ebf7ffdda1e48fc106c859d61507
Dmytro Ostrytskyi <dmytro.ostrytskyi@nordstrom.com>
2026-05-14 17:39:26 +0300

    Adding version

--- Rover/version.h
+++ Rover/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduRover V4.6.3"
+#define THISFIRMWARE "ArduRover V4.6.3.1-custom"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,6,3,FIRMWARE_VERSION_TYPE_OFFICIAL
